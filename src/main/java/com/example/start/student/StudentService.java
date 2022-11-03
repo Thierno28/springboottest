638eec1ac88c86beed20989d72d22a3bbdf2b9e8
Thierno28 <thiernodiop@esp.sn>
2022-11-03 06:55:38 +0000

    first commit

---
+++ src/main/java/com/example/start/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.start.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+ 
+    }
+
+}
